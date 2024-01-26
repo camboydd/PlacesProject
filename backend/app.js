5499e3f4d04837319c610b6cf199dddd3295830a
camboydd <98326906+camboydd@users.noreply.github.com>
2024-01-26 16:29:42 -0500

    committing environment variables in backend

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect('mongodb+srv://camboydd:Dudesrule27@clustermernproject.ucp5clz.mongodb.net/mern?retryWrites=true&w=majority')
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clustermernproject.ucp5clz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(3000)
     })
--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyBkPHLtLQAHkhv5zOS7V9qgPRBFIXJS1PQ';
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   // return {
