7fcc780416872fa7e0c5389be0d987a32650b6d2
camboydd <98326906+camboydd@users.noreply.github.com>
2024-01-28 14:15:36 -0500

    Deployed backend to Heroku server and frontend to firebase

--- backend/app.js
+++ backend/app.js
@@ -54,7 +54,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clustermernproject.ucp5clz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
-        app.listen(3000)
+        app.listen(process.env.PORT || 3000)
     })
     .catch(err => {
         console.log(err);
